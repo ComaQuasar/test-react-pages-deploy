e2102d1597d07290e57b2647680f67460455a192
ComaQuasar <ario_n@hotmail.co.uk>
2023-03-31 10:04:21 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <img src={process.env.REACT_APP_IMAGE_URL} alt="test" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
